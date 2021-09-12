9699c931889864db2601405f760081a314a5cc13
Riki <43941751+curest0x1021@users.noreply.github.com>
2021-09-12 22:39:22 +0900

    Added theme control

--- src/config/Layout.js
+++ src/config/Layout.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Box, Container } from "@material-ui/core";
+import { Box, Container, FormControlLabel, FormGroup, Switch } from "@material-ui/core";
 import { useStyle } from "../theme/styles/layout";
+import { useTheme } from "../hooks";
 import Students from "../pages/Students";
 
 const Layout = () => {
   const classes = useStyle();
-
+  const { theme, toggleThemeF } = useTheme();
   return (
     <Box className={classes.mainContainer}>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={theme === "dark"}
+              onChange={toggleThemeF}
+              color="primary" />
+          }
+          label="Dark?"
+        />
+      </FormGroup>
       <Container className={classes.container} maxWidth="md">
         <Students />
       </Container>
